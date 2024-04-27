0b6f4d8eb30af9d53a9ccf0131b9481c7ef73c7a
PM2600 <3262929659@qq.com>
2024-04-27 20:11:30 +0800

    src

---
+++ weather-forecast/mainwindow.cpp
@@ -0,0 +1,44 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMouseEvent>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //设置窗口属性为没有边框的窗口
+    setWindowFlag(Qt::FramelessWindowHint);
+    //固定窗口大小
+    setFixedSize(width(), height());
+    exitMenu = new QMenu(this);
+    exitAct = new QAction;
+    exitAct->setText(u8"退出");
+    exitAct->setIcon(QIcon(":/weatherIco/close.ico"));
+    exitMenu->addAction(exitAct);
+    connect(exitAct, SIGNAL(triggered(bool)), this, SLOT(slot_exitApp()));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::contextMenuEvent(QContextMenuEvent* event){
+    exitMenu->exec(QCursor::pos());
+    event->accept();
+}
+
+void MainWindow::slot_exitApp(){
+    qApp->exit(0);
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent* event){
+    move(event->globalPos() - mPos);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent* event){
+    mPos = event->globalPos() - this->pos();
+}
